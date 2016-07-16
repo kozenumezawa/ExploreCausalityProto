6ca31fb815e48d7a977b1fff5bce140532c9e306
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-16 23:25:10 +0900

    Initial commit

---
+++ src/components/output-space.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import THREE from 'three'
+const OrbitControls = require('three-orbit-controls')(THREE);
+
+
+export default class OutputSpace extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.threeRender = this.threeRender.bind(this);
+  }
+
+  //  初期描画が発生した直後に実行される
+  componentDidMount() {
+    const width = 500;
+    const height = 500;
+
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.WebGLRenderer();
+    const pixel_ratio = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
+    this.renderer.setPixelRatio(pixel_ratio);
+    this.renderer.setSize(width, height);
+
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000 );
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    document.getElementById('output_space').appendChild(this.renderer.domElement);
+
+    const geometry = new THREE.CubeGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+    this.camera.position.z = 5;
+
+    this.threeRender();
+  }
+
+  threeRender() {
+    requestAnimationFrame(this.threeRender);
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  render() {
+    return (
+      <div id="output_space">
+      </div>
+    );
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import OutputSpace from './components/output-space'
+
+
+export default class main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <OutputSpace />
+      </div>
+    );
+  }
+}
+
